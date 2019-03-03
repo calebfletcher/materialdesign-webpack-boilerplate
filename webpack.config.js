d29d1d9d62474fcb1ccc5cc12ea63cd2d87d6930
Caleb Fletcher <caleb@fletcher.cf>
2019-03-03 23:14:22 +1100

    Update Babel

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ module.exports = {
         test: /\.js$/,
         loader: 'babel-loader',
         query: {
-          presets: ['es2015'],
+          presets: ['env'],
           plugins: ['transform-object-assign']
         },
       }
